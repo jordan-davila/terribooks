3fd30a1adb66d6467b8f6bb0fb6d5b464b444297
Jordan Davila <hi@jordandavila.com>
2021-01-28 15:12:09 -0500

    Modify CSS and Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,43 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
+    purge: ["./vendor/laravel/jetstream/**/*.blade.php", "./storage/framework/views/*.php", "./resources/views/**/*.blade.php", "./resources/js/**/*.vue"],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans]
             },
+            fontSize: {
+                xxs: [".65rem", "1rem"]
+            },
+            colors: {
+                gray: {
+                    300: "#a0aec0"
+                }
+            }
         },
         customForms: theme => ({
             default: {
-              input: {
-                borderWidth: theme('borderWidth.0'),
-                backgroundColor: theme('colors.gray.200'),
-              },
-            },
-          })
+                input: {
+                    borderWidth: theme("borderWidth.0"),
+                    backgroundColor: theme("colors.gray.200")
+                }
+            }
+        })
     },
 
     variants: {
         extend: {
-            opacity: ['disabled'],
-            borderColor: ['focus'],
-            borderWidth: ['focus', 'hover'],
-        },
+            opacity: ["disabled"],
+            borderColor: ["focus"],
+            borderWidth: ["focus", "hover"]
+        }
     },
 
     plugins: [
         // Not yet configurable, for now just use forms.scss
         // require('@tailwindcss/forms'),
-        require('@tailwindcss/typography')
-    ],
+        require("@tailwindcss/typography")
+    ]
 };
