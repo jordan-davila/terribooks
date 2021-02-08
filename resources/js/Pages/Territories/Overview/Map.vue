e3efe7279a86f475bfaceecafaf444ee86bf74f4
Jordan Davila <hi@jordandavila.com>
2021-02-07 21:38:26 -0500

    Enhance Overview Page

---
+++ resources/js/Pages/Territories/Overview/Map.vue
@@ -0,0 +1,38 @@
+<template lang="">
+    <div class="w-full h-full rounded-lg shadow-lg overflow-hidden bg-white">
+        <l-map ref="map" :zoom="zoom" :center="center" :options="options" class="w-full h-full">
+            <l-tile-layer :url="url" :options="options_tile" />
+            <l-geo-json
+                ref="geojson"
+                @ready="getBoundsJSON()"
+                :geojson="getGeoJSONCoordinates()"
+                :options-style="styles.purple"
+            />
+        </l-map>
+    </div>
+</template>
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LGeoJson } from "vue2-leaflet";
+import { MapMixin } from "@/Pages/Territories/Mixins/Map";
+
+export default {
+    mixins: [MapMixin],
+    components: { LMap, LTileLayer, LGeoJson },
+
+    updated() {
+        this.getBoundsJSON();
+    },
+
+    methods: {
+        getBoundsJSON(feature, layer) {
+            this.$refs.map.fitBounds(this.$refs.geojson.getBounds());
+        },
+        getGeoJSONCoordinates() {
+            return JSON.parse(
+                `{ "type": "FeatureCollection", "features": [ { "type": "Feature", "properties": {}, "geometry": { "type": "Polygon", "coordinates": ${this.$page.props.territory.data.coordinates} } } ] }`
+            );
+        }
+    }
+};
+</script>
--- resources/js/Pages/Territories/Overview/Show.vue
+++ resources/js/Pages/Territories/Overview/Show.vue
@@ -7,8 +7,90 @@
             <!-- Territory Picker -->
             <TerritoryPicker />
             <div class="flex flex-1 relative">
-                <div class="flex flex-col flex-1 overflow-y-auto relative w-full">
-                    {{ territory.data.code }}
+                <div class="flex flex-1 relative w-full h-full p-8">
+                    <div
+                        class="overview absolute top-14 left-14 w-80 bg-white shadow-lg rounded-lg p-8"
+                        style="z-index: 998"
+                    >
+                        <div
+                            class="text-xxs text-gray-300 mb-4 w-full uppercase font-bold flex justify-between items-center"
+                        >
+                            <span>Overview</span>
+                            <button class="w-6 h-6 text-gray-300 text-sm">
+                                <i class="fas fa-caret-down"></i>
+                            </button>
+                        </div>
+                        <div class="territory-info flex items-center mb-8">
+                            <div
+                                class="w-10 h-10 flex justify-center items-center text-xs rounded-lg text-indigo-600 bg-indigo-100 shadow-lg font-bold mr-4"
+                            >
+                                {{ $page.props.territory.data.code }}
+                            </div>
+                            <div
+                                class="details text-xxs text-gray-300 uppercase font-bold flex flex-col"
+                            >
+                                <span>
+                                    {{
+                                        $page.props.territory.data.city_name +
+                                            " " +
+                                            $page.props.territory.data.order
+                                    }}
+                                </span>
+                                <span class="opacity-60">No Current Assignee</span>
+                            </div>
+                        </div>
+                        <div class="territory-stats flex items-center">
+                            <div
+                                class="cards flex flex-col rounded-lg bg-gray-100 w-20 h-20 p-3 mr-4 justify-center items-center"
+                            >
+                                <div class="uppercase font-bold text-xxs text-gray-400 mb-2">
+                                    House #
+                                </div>
+                                <div
+                                    class="circle rounded-full bg-white flex justify-center items-center"
+                                >
+                                    <span
+                                        class="text-gray-400 font-semibold text-sm h-8 w-8 flex justify-center items-center"
+                                    >
+                                        {{ $page.props.territory.data.house_count }}
+                                    </span>
+                                </div>
+                            </div>
+                            <div
+                                class="cards flex flex-col rounded-lg bg-gray-100 w-20 h-20 p-3 mr-4 justify-center items-center"
+                            >
+                                <div class="uppercase font-bold text-xxs text-gray-400 mb-2">
+                                    PHONE #
+                                </div>
+                                <div
+                                    class="circle rounded-full bg-white flex justify-center items-center"
+                                >
+                                    <span
+                                        class="text-gray-400 font-semibold text-sm h-8 w-8 flex justify-center items-center"
+                                    >
+                                        {{ $page.props.territory.data.phone_count }}
+                                    </span>
+                                </div>
+                            </div>
+                            <div
+                                class="cards flex flex-col rounded-lg bg-gray-100 w-20 h-20 p-3 justify-center items-center"
+                            >
+                                <div class="uppercase font-bold text-xxs text-gray-400 mb-2">
+                                    BIZ #
+                                </div>
+                                <div
+                                    class="circle rounded-full bg-white flex justify-center items-center"
+                                >
+                                    <span
+                                        class="text-gray-400 font-semibold text-sm h-8 w-8 flex justify-center items-center"
+                                    >
+                                        {{ $page.props.territory.data.business_count }}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Map />
                 </div>
             </div>
         </div>
@@ -18,13 +100,15 @@
 import AppLayout from "@/Layouts/AppLayout";
 import TerritoryPicker from "@/Pages/Territories/Components/TerritoryPicker";
 import NavSub from "@/Pages/Territories/Components/NavSub";
+import Map from "@/Pages/Territories/Overview/Map";
 
 export default {
     props: ["territory"],
     components: {
         AppLayout,
         TerritoryPicker,
-        NavSub
+        NavSub,
+        Map
     }
 };
 </script>
